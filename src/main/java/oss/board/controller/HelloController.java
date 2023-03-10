f7a64848fdda09844974f2b976f1d8f44e7564f1
ohsangsoo <tkdtn3264@gmail.com>
2023-03-10 18:50:21 +0900

    API 연습하기

--- src/main/java/oss/board/controller/HelloController.java
+++ src/main/java/oss/board/controller/HelloController.java
@@ -3,7 +3,9 @@ package oss.board.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -16,6 +18,33 @@ public class HelloController {
     public String helloMvc(@RequestParam(required = false,value = "name")String name, Model model){
         model.addAttribute("name", name);
         return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name")String name){
+        return "hello"+name;
+        // 뷰없이 문자그대로 값이 넘어가서 보여준다.
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name")String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    // api -> Json 형태로 나오게 되며 {key:  values} 으로 반환된다.
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
 }
