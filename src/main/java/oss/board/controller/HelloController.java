f8e198e71a369387786279808c6e0f2fff445a49
ohsangsoo <tkdtn3264@gmail.com>
2023-03-10 18:31:28 +0900

    mvc와 템플릿 엔진

--- src/main/java/oss/board/controller/HelloController.java
+++ src/main/java/oss/board/controller/HelloController.java
@@ -3,6 +3,7 @@ package oss.board.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,6 +11,11 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data","한글도 되니~!");
         return "hello";
-
     }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(required = false,value = "name")String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
 }
