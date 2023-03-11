57bf0f9a06e14c56f4c27e43330c6fe83080a6dc
ohsangsoo <tkdtn3264@gmail.com>
2023-03-11 15:20:54 +0900

    회원 리포지토리 테스트케이스 Member와 result 결과가 null인경우

--- src/test/java/oss/board/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/oss/board/repository/MemoryMemberRepositoryTest.java
@@ -14,7 +14,7 @@ public class MemoryMemberRepositoryTest {
         repository.save(member);
         Member result = repository.findById(member.getId()).get();
         //System.out.println("result = " + (result == member)); // 내가 새로 만든 것과 db에있느것이 같은지를 확인한다.
-        Assertions.assertEquals(member, result);
+        Assertions.assertEquals(member, null);
     }
 }
  
