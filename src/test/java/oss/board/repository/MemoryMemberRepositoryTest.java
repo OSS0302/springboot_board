ce3451968c479f2a00ed5e2c1a698ec2464f9c14
ohsangsoo <tkdtn3264@gmail.com>
2023-03-11 15:24:52 +0900

    회원 리포지토리 테스트케이스 요즘에는 Member와 result assertThat(member).isEqualTo(result);를 쓴다.

--- src/test/java/oss/board/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/oss/board/repository/MemoryMemberRepositoryTest.java
@@ -1,8 +1,11 @@
 package oss.board.repository;
 
-import org.junit.jupiter.api.Assertions;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import oss.board.domain.Member;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class MemoryMemberRepositoryTest {
     MemberRepository repository = new MemoryMemberRepository();
@@ -13,8 +16,12 @@ public class MemoryMemberRepositoryTest {
         member.setName("spring");
         repository.save(member);
         Member result = repository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+
+
         //System.out.println("result = " + (result == member)); // 내가 새로 만든 것과 db에있느것이 같은지를 확인한다.
-        Assertions.assertEquals(member, null);
+        //Assertions.assertEquals(member, null);
+
     }
 }
  
