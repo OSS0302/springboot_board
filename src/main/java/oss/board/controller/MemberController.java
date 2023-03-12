b685cc962f84cf4b4acabc27f012c65e0f8db8bf
ohsangsoo <tkdtn3264@gmail.com>
2023-03-12 17:59:54 +0900

    회원 웹 기능 -회원 등록하기

--- src/main/java/oss/board/controller/MemberController.java
+++ src/main/java/oss/board/controller/MemberController.java
@@ -2,7 +2,10 @@ package oss.board.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import oss.board.Service.MemberService;
+import oss.board.domain.Member;
 
 @Controller
 public class MemberController {
@@ -12,4 +15,18 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+    @GetMapping("/members/new")
+    public String creaeForm(){
+        return "members/createMemberForm";
+    }
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName()); // 이름 가져와라
+        memberService.join(member); // 회원가입 이름을 넘겨줘라.
+
+        System.out.println("member.getName() = " + member.getName());
+        return "redict:/"; // 회원 가입이 끝나면 홈 화면으로 보여줘라
+    }
+
 }
---
+++ src/main/java/oss/board/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package oss.board.controller;
+
+public class MemberForm {
+    private String name; // 회원 이름
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
