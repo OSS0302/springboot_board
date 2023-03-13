9ae2184db08d84b705160ad8c155159b66a381a0
ohsangsoo <tkdtn3264@gmail.com>
2023-03-13 18:09:00 +0900

    스프링 JdbcTemplate

--- src/main/java/oss/board/repository/JdbcTemplateMemberRepository.java
+++ src/main/java/oss/board/repository/JdbcTemplateMemberRepository.java
@@ -1,4 +1,70 @@
 package oss.board.repository;
 
-public class JdbcTemplateMemberRepository {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import oss.board.domain.Member;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class JdbcTemplateMemberRepository  implements  MemberRepository {
+
+    public final JdbcTemplate jdbcTemplate;
+    // 1. JdbcTemplate의 생성자 생성후
+    // 2. JdbcTemplate인젝션에서 디비 인젝션으로 연동한다.
+
+    //@Autowired 참고로 생성자가 하나면 스프링빈으로 등록될때 Autowired 어노테이션을 생략하 가능하다.
+    // 데이터 소스가 자동으로 인젝션을 한다.
+    public JdbcTemplateMemberRepository(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("member").usingGeneratedKeyColumns("id");
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name", member.getName());
+        // 인텔리제이에서 코드를 짜면 자동으로 insert into member(name) values(?)를 쿼리문을 써줄 필요없이 만들어준다.
+    Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+        member.setId(key.longValue());
+        return member;
+}
+
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        List<Member> result = jdbcTemplate.query("select * from member where id = ?", memberRowMapper(),id);
+        return result.stream().findAny();
+    }
+
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> result = jdbcTemplate.query("select * from member where name = ?", memberRowMapper(), name);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return jdbcTemplate.query("select * from member", memberRowMapper()); //리스트로 반환
+    }
+
+    private RowMapper<Member> memberRowMapper() {
+        return (rs, rowNum) -> {
+            Member member = new Member();
+            member.setId(rs.getLong("id"));
+            member.setName(rs.getString("name"));
+            return member;
+        }; }
 }
