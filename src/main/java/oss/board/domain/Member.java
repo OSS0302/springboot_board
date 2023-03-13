6cae5586e32599f96492f2c9d8d7f79ac61cd76a
ohsangsoo <tkdtn3264@gmail.com>
2023-03-13 21:35:19 +0900

    @Entity 맵핑하기

--- src/main/java/oss/board/domain/Member.java
+++ src/main/java/oss/board/domain/Member.java
@@ -1,7 +1,13 @@
 package oss.board.domain;
 
+import javax.persistence.*;
+
+@Entity
+
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // db가 자동으로 생성하는것을 IDENTITY라고한다.
     private Long id; // 회원 아이디
+    //@Column(name="username") @Column(name="username")라고하면 db에있는 컬럼 username과 맵핑된다.
     private String name; //회원 이름
 
     public Long getId() {
