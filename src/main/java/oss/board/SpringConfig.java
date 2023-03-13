a72efc09bb47d2e2ec3d526cf9d055287d7d6eda
ohsangsoo <tkdtn3264@gmail.com>
2023-03-13 21:36:45 +0900

    JPA 자바빈에 등록하기

--- src/main/java/oss/board/SpringConfig.java
+++ src/main/java/oss/board/SpringConfig.java
@@ -4,20 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import oss.board.Service.MemberService;
-import oss.board.repository.JdbcMemberRepository;
-import oss.board.repository.JdbcTemplateMemberRepository;
-import oss.board.repository.MemberRepository;
-import oss.board.repository.MemoryMemberRepository;
+import oss.board.repository.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    // db와 연동하기 위해서 데이터소스 만든후에 생성자 추가후 Autowired을 만든다.
-    private DataSource dataSource;
+//    // db와 연동하기 위해서 데이터소스 만든후에 생성자 추가후 Autowired을 만든다.
+//    private DataSource dataSource;
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+    //@PersistenceContext 원래 스펙에서는 @PersistenceContext 써야되지만 스프링부트에서는 자동으로 의존성 주입이 됩니다.
+    private EntityManager em;
+
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -29,6 +35,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
         //return  new JdbcMemberRepository(dataSource); 20년전 방식
-        return new JdbcTemplateMemberRepository(dataSource); //스프링 JdbcTemplate
+        //return new JdbcTemplateMemberRepository(dataSource); //스프링 JdbcTemplate
+        return  new JPAMemberRepository(em);
     }
 }
