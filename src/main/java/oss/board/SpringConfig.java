d72af369a82800c81d4cc4137edae10b886b73c2
ohsangsoo <tkdtn3264@gmail.com>
2023-03-14 14:50:36 +0900

    스프링데이터 JPA 자바빈등록 하기

--- src/main/java/oss/board/SpringConfig.java
+++ src/main/java/oss/board/SpringConfig.java
@@ -12,30 +12,35 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-//    // db와 연동하기 위해서 데이터소스 만든후에 생성자 추가후 Autowired을 만든다.
+//    // db와 연동하기 위해서 데이터소스 만든후에 생성자 추가후 Autowired을 만든다. 20년전 방식
 //    private DataSource dataSource;
 //    @Autowired
 //    public SpringConfig(DataSource dataSource) {
 //        this.dataSource = dataSource;
 //    }
     //@PersistenceContext 원래 스펙에서는 @PersistenceContext 써야되지만 스프링부트에서는 자동으로 의존성 주입이 됩니다.
-    private EntityManager em;
 
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+    private final MemberRepository memberRepository;
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService(){
-
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-    @Bean
-    public MemberRepository memberRepository() {
-        //return new MemoryMemberRepository();
-        //return  new JdbcMemberRepository(dataSource); 20년전 방식
-        //return new JdbcTemplateMemberRepository(dataSource); //스프링 JdbcTemplate
-        return  new JPAMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//        return  new JdbcMemberRepository(dataSource); 20년전 방식
+//        return new JdbcTemplateMemberRepository(dataSource); //스프링 JdbcTemplate
+//        return  new JPAMemberRepository(em); JPA
+//    }
 }
