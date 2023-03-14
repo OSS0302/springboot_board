e801f98d16a44a42bc8bf13e5f4fa25860739263
ohsangsoo <tkdtn3264@gmail.com>
2023-03-14 16:37:09 +0900

    AOP 원하는 곳에 공통 관심사항 적용(타게팅)

--- src/main/java/oss/board/aop/TimeTraceAop.java
+++ src/main/java/oss/board/aop/TimeTraceAop.java
@@ -2,14 +2,17 @@ package oss.board.aop;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
 
 import java.awt.*;
 
 @Aspect
-//@Component // 자바 빈에 등록해줘야해서 컴포넌트 어노테이션을 써도되고
+@Component// 자바 빈에 등록해줘야해서 컴포넌트 어노테이션을 써도되고
 public class TimeTraceAop {
+    @Around("execution(* oss.board.repository.*(..))")// 패키지명  //원하는 곳에 공통 관심사항 적용(타게팅)하기
     public Object execute(ProceedingJoinPoint joinPoint ) throws Throwable {//예외가 터지면 던진다.
         long start = System.currentTimeMillis(); // 시작시간 측정
         System.out.println("START:"+joinPoint.toString());//여기 안에 어떤 메서드를 콜하는 확인
